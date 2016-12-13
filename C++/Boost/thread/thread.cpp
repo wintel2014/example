1fb52190cfe40a0227c35003d3b57c001566080c
Wang Jiazi <jiazi.wang@slamtec.com>
2016-12-13 10:00:20 +0800

    Use the mem_fn to join the target thread

--- C++/Boost/thread/thread.cpp
+++ C++/Boost/thread/thread.cpp
@@ -2,12 +2,13 @@
 #include <boost/thread/thread.hpp>
 #include <vector>
 #include <iostream>
-#include <memory>
+//#include <memory>
 void hello()
 {
     std::cout<<"hello,boost thread lib."<<std::endl;
     sleep(1);
 }
+
 int main()
 {
     const int MaxThreads = 100;
@@ -15,10 +16,14 @@ int main()
 
     for(int i=0; i<MaxThreads; i++)
        ThreadVec.push_back(std::make_shared<boost::thread>(hello));
+#if 0
     for(auto it:ThreadVec) {
         if(it->joinable())
             it->join();
         std::cout<<"Join"<<std::endl;
     }
+#else
+    for_each(ThreadVec.begin(), ThreadVec.end(), mem_fn(&boost::thread::join));
+#endif
     return 0 ;
 }
