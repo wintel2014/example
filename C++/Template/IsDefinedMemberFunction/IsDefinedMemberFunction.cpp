005049731862a5a6a847d8d6e0251bacfe4452e7
Wang Jiazi <jiazi.wang@slamtec.com>
2017-04-24 10:20:06 +0800

    compilation error! class template can't deduce argument The specialization can get the type of the argument

--- C++/Template/IsDefinedMemberFunction/IsDefinedMemberFunction.cpp
+++ C++/Template/IsDefinedMemberFunction/IsDefinedMemberFunction.cpp
@@ -1,27 +1,40 @@
 #include <iostream>
+#include <type_traits>
+#include<vector>
+
 using namespace std;
+
 class Test {
 public:
-    bool Add(long) {cout<<"Test::Add\n";}
+ //   bool Add(long) {cout<<"Test::Add\n";}
     bool Add(int) {cout<<"Test::Add\n";}
+};
+
+class Empty {
 };
 
 template<typename T>
 struct is_defined_member_function {
     const static bool value = false;
 };
-
-template <typename T>
-struct helper {
-    typedef typename T::Add type;
-};
-template<typename T>
-struct is_defined_member_function2: public helper<T> {
+template<>
+struct is_defined_member_function<Test> {
     const static bool value = true;
 };
 
-template<>
-struct is_defined_member_function<Test> {
+/*
+template<typename T>
+struct is_defined_member_function<typename T::Add> {
+    typedef typename T::Add type;
+    const static bool value = true;
+};
+*/
+template <typename T>
+struct helper {
+    typedef typename T::Add type; //It doesn't work for member function
+};
+template<typename T>
+struct is_defined_member_function2: public helper<T> {
     const static bool value = true;
 };
 
@@ -29,7 +42,7 @@ struct is_defined_member_function<Test> {
 template<class T>  
 struct is_defined_member_function3
 {  
-    static void Add();  
+    static bool Add(int);  
     struct traits_helper :public T  
     {  
         static int f(bool (T::*)(int));  
@@ -42,8 +55,51 @@ struct is_defined_member_function3
     enum {value = traits_helper::value};  
 }; 
 
+
+//OK 
+template<typename T>
+struct is_defined_member_function4
+{
+
+    template<typename U, bool (U::*)(int)>
+    struct traits_helper;
+    
+    template<typename U>
+    static int f(traits_helper<U, &U::Add>*);
+    template<typename U>
+    static char f(...);
+
+    enum {value = sizeof(f<T>(0)) == sizeof(int)};
+    const static bool defined = sizeof(f<T>(0)) == sizeof(int);
+};
+
+template<typename T>
+struct is_defined_member_function5
+{
+    enum { value=123};
+};
+
+
+template<typename U, bool (U::*)(int)>
+struct traits_helper;
+
+template<typename T>
+struct is_defined_member_function5<traits_helper<T, &T::Add> >
+{
+    enum {value=1};
+};
+
 int main()
 {
     cout<<is_defined_member_function<Test>::value<<endl;
     cout<<is_defined_member_function3<Test>::value<<endl;
+    //cout<<is_defined_member_function3<Empty>::value<<endl; //Compile ERROR
+
+    cout<<"================================================\n";
+    cout<<is_defined_member_function4<Test>::value<<endl;
+    cout<<is_defined_member_function4<Empty>::value<<endl;
+
+    cout<<"================================================\n";
+    cout<<is_defined_member_function5<traits_helper<Test, &Test::Add>>::value<<endl;
+    cout<<is_defined_member_function5<Empty>::value<<endl;
 }
