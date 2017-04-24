0b4862f96960e5b200989adbecece2096c6459c4
Wang Jiazi <jiazi.wang@slamtec.com>
2017-04-24 17:51:23 +0800

    test.cpp:33:6: note:   template argument deduction/substitution failed: test.cpp:45:14: note:   deduced conflicting types for parameter ‘T’ (‘Base’ and ‘Derive’)      show(B, D);
    Template argument deduction doesn't support implicit downward-cast

--- C++/Template/TemplateParameterDeduction/test.cpp
+++ C++/Template/TemplateParameterDeduction/test.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <typeinfo>
 using namespace std;
 
 
@@ -19,9 +20,33 @@ int add (int a, int b)
     return 1234;
 }
 
+
+class Base
+{
+};
+
+class Derive: public Base
+{
+};
+
+template<typename T>
+void show(T a, T b)
+{
+    std::cout<<typeid(a).name()<<endl;
+}
+
+template<typename T, typename U>
+void show(T a, U b)
+{
+    std::cout<<typeid(a).name()<<"  "<<typeid(U).name()<<endl;
+}
 int main()
 {
     int a=1;
     double b=2;
     cout<<add(a,b)<<std::endl;
+
+    Base B;
+    Derive D;
+    show(B, D);
 }
