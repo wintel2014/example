86fc54e2560db78a1e1a7c70a5644d99122da131
Wang Jiazi <jiazi.wang@slamtec.com>
2017-05-04 17:01:46 +0800

    add new APIs

--- C++/Json/simple/json.cpp
+++ C++/Json/simple/json.cpp
@@ -11,7 +11,7 @@ int ParseJsonFromString()
   {  
     std::string upload_id = root["uploadid"].asString();  // 访问节点，upload_id = "UP000000"   
     int code = root["code"].asInt();    // 访问节点，code = 100   
-    cout<<code<<endl;
+    cout<<upload_id<<":"<<code<<"   isArray="<<root.isArray()<<endl;
   }  
   return 0;  
 } 
@@ -19,5 +19,36 @@ int ParseJsonFromString()
 int main()
 {
     ParseJsonFromString();
+
+    Json::Value root=string("JsonStringConstructor");
+    cout<<root.asString()<<endl;
+
+
+    Json::Value array;
+    Json::Value left;
+    Json::Value middle;
+    Json::Value right;
+
+    left["1"]="L1";
+    left["2"]="L2";
+
+    middle["1"]="M1";
+    middle["2"]="M3";
+    middle["3"]="M3";
+
+    array[1]=left;
+    array[2]=left;
+    array[3]=middle;
+    array[4]=right;
+    array[9]=right;
+    array[10]=right;
+    array[10]=left;
+
+    cout<<array.isArray()<<"  arraysize="<<array.size()<<"  "<<left.isArray()<<" empty="<<left.empty()<<"  empty="<<right.empty()<<endl;
+
+    for(int index=0; index<array.size(); index++)
+    {
+        cout<<"array[index].empty="<<array[index].empty()<<endl;
+    }
 }
 
