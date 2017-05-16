2958786eaf896e6c4916cffdbbf1768a2565dd02
Wang Jiazi <jiazi.wang@slamtec.com>
2017-05-16 15:12:05 +0800

    remove trailing space

--- C++/Boost/FileLock/file_lock.cpp
+++ C++/Boost/FileLock/file_lock.cpp
@@ -1,38 +1,38 @@
 #include <fstream>
-#include <iostream> 
-#include <boost/interprocess/sync/file_lock.hpp> 
-#include <boost/thread/lock_guard.hpp> 
+#include <iostream>
+#include <boost/interprocess/sync/file_lock.hpp>
+#include <boost/thread/lock_guard.hpp>
 #include <cstdlib>
 
-int main() 
-{ 
-  using namespace boost::interprocess; 
-  std::string fileName("test"); 
+int main()
+{
+  using namespace boost::interprocess;
+  std::string fileName("test");
   std::fstream file;
 
-  file.open(fileName.c_str(), std::ios::out | std::ios::binary | std::ios::trunc); 
+  file.open(fileName.c_str(), std::ios::out | std::ios::binary | std::ios::trunc);
 
-  if (!file.is_open() || file.bad()) 
-  {   
-    std::cout << "Open failed" << std::endl; 
-    exit(-1); 
+  if (!file.is_open() || file.bad())
+  {
+    std::cout << "Open failed" << std::endl;
+    exit(-1);
   }
   std::cout << "Process 1 open file" << std::endl;
 
-  try { 
+  try {
     file_lock f_lock(fileName.c_str());
     boost::lock_guard<file_lock>  guard(f_lock);
 
     std::cout << "Locked in Process 1" << std::endl;
-    file.write("Process 1\n", 9); 
-    file.flush(); 
+    file.write("Process 1\n", 9);
+    file.flush();
 
     sleep(15);
     std::cout << "Unlocked from Process 1" << std::endl;
-  } catch (interprocess_exception& e) { 
+  } catch (interprocess_exception& e) {
     std::cout << e.what( ) << std::endl;
   }
 
   file.close();
-  return 0;  
+  return 0;
 }
--- C++/Boost/FileLock/file_try_lock.cpp
+++ C++/Boost/FileLock/file_try_lock.cpp
@@ -1,34 +1,34 @@
 #include <fstream>
-#include <iostream> 
-#include <boost/interprocess/sync/file_lock.hpp> 
-#include <boost/thread/lock_guard.hpp> 
+#include <iostream>
+#include <boost/interprocess/sync/file_lock.hpp>
+#include <boost/thread/lock_guard.hpp>
 #include <cstdlib>
 
-int main() 
-{ 
-  using namespace boost::interprocess; 
-  std::string fileName("test"); 
+int main()
+{
+  using namespace boost::interprocess;
+  std::string fileName("test");
   std::fstream file;
 
-  file.open(fileName.c_str(), std::ios::out | std::ios::binary | std::ios::trunc); 
+  file.open(fileName.c_str(), std::ios::out | std::ios::binary | std::ios::trunc);
 
-  if (!file.is_open() || file.bad()) 
-  {   
-    std::cout << "Open failed" << std::endl; 
-    exit(-1); 
+  if (!file.is_open() || file.bad())
+  {
+    std::cout << "Open failed" << std::endl;
+    exit(-1);
   }
 
-  try { 
+  try {
     file_lock f_lock(fileName.c_str());
 
     std::cout<<"TryLock ="<<f_lock.try_lock()<<"\n";
     sleep(15);
     f_lock.unlock();
 
-  } catch (interprocess_exception& e) { 
+  } catch (interprocess_exception& e) {
     std::cout << e.what( ) << std::endl;
   }
 
   file.close();
-  return 0;  
+  return 0;
 }
