56aea749de589681e8533f28be82718d77c3c16c
Wang Jiazi <jiazi.wang@slamtec.com>
2017-06-15 15:13:37 +0800

    mem_fun and bind

--- C++/Boost/thread2/test.cpp
+++ C++/Boost/thread2/test.cpp
@@ -1,4 +1,12 @@
-void do_work(unsigned id);
+#include <vector>
+#include <thread>
+#include <algorithm>
+#include <iostream>
+
+void do_work(unsigned id)
+{
+    std::cout<<id<<std::endl;
+}
 
 void f()
 {
@@ -7,6 +15,14 @@ void f()
   {
     threads.push_back(std::thread(do_work,i)); // 产生线程
   } 
-  std::for_each(threads.begin(),threads.end(),
-                  std::mem_fn(&std::thread::join)); // 对每个线程调用join()
+#if 0
+  std::for_each(threads.begin(),threads.end(),std::mem_fn(&std::thread::join)); // 对每个线程调用join()
+#else
+  std::for_each(threads.begin(),threads.end(),std::bind(&std::thread::join, std::placeholders::_1)); // 对每个线程调用join()
+#endif
+}
+
+int main()
+{
+    f();
 }
