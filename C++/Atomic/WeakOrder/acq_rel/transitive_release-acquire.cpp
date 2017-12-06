b194be2c558560638bf3524cb5e270b867ca0fce
wintel2014 <jiazi.wang@dfc.sh>
2017-12-06 15:29:09 +0800

    Modify the file according to C++ reference
    Maybe X86 can guarantee the SS/LS/LL order

--- C++/Atomic/WeakOrder/acq_rel/transitive_release-acquire.cpp
+++ C++/Atomic/WeakOrder/acq_rel/transitive_release-acquire.cpp
@@ -3,9 +3,15 @@
 #include <cassert>
 #include <vector>
  
+#include <thread>
+#include <atomic>
+#include <cassert>
+#include <vector>
+ 
 std::vector<int> data;
 std::atomic<int> flag = {0};
  
+#ifdef ARCH_X86 
 void thread_1()
 {
     data.push_back(42);
@@ -26,9 +32,42 @@ void thread_3()
         ;
     assert(data.at(0) == 42); // will never fire
 }
+#else 
+void thread_1()
+{
+    data.push_back(42);
+    flag.store(1, std::memory_order_release);
+}
  
+void thread_2()
+{
+    int expected=1;
+    while (!flag.compare_exchange_strong(expected, 2, std::memory_order_acq_rel)) {
+        expected = 1;
+    }
+}
+ 
+void thread_3()
+{
+    while (flag.load(std::memory_order_acquire) < 2)
+        ;
+    assert(data.at(0) == 42); // will never fire
+}
+#endif 
+long long func(int i)
+{
+    long long d=0x1234567812345678;
+    long long d2=0x1234567812345678;
+    long long d3=0x1234567812345678;
+    return i+d+d2+d3;
+}
+struct ZeroInit{
+    long long data;
+    std::atomic<bool> false_init;
+};
 int main()
 {
+    ZeroInit* zero = new ZeroInit();
     std::thread a(thread_1);
     std::thread b(thread_2);
     std::thread c(thread_3);
